9214613b1af774389b05a05f450616d956085170
Keima Senpai <98184310+KeimaSenpai@users.noreply.github.com>
2022-04-18 09:48:02 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Program-Plus-Channel
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ bot/constants.py
@@ -0,0 +1,19 @@
+# Admnistradores
+# Para añadir más separalos por comas
+administradores = [
+    5070319912, 1618347551
+    ]
+
+# Canal a donde se envia la plantilla
+# Se debe incluir el ' - '
+CHANNEL = -1001709666769
+
+# Estados ConversationHandler
+PHOTO = 0
+NOMBRE = 1
+LINK = 2
+DESCRIPCION = 3
+CONTENIDO_SUBIR = 4
+PALABRA_CREADOR = 5
+VALORACION = 6
+SEND = 7
